f5b13f1a277edcf0d40fbd953b37fa653fba7623
kinderswan <gamanovichpavel@gmail.com>
2017-02-10 07:43:26 -0600

    fourth

--- report.js
+++ report.js
@@ -1,11 +1,10 @@
-this.Main = function(){
-	
-}
-Main.prototype.loadCode =  function(event, data){
+class Main {
+	loadCode(event, data){
 		var callback = event.data.callback;
-		var filenameUrl = $(".cppFile").val();
-		var ext = this.getFileExtension(filenameUrl);
-		if(url === ""){
+		var context = event.data.context;
+		var filenameUrl = $(".codeFile").val();
+		var ext = context.getFileExtension(filenameUrl);
+		if(filenameUrl === ""){
 			return;
 		}
 		$.ajax({
@@ -20,25 +19,29 @@ Main.prototype.loadCode =  function(event, data){
 				$(".forms").remove();
 			}
 		});
-}
+		}
 
-Main.prototype.fulfillSVGs = function(){
-	var files = $(".svgFiles").val().split(";");
-	if(files.length === 0){
-		return;
-	}
-	for(var i = 0; i < files.length; i++)
-	{
-		var j = i;
-		$(".images").append("<img src='"+files[j]+"'/></br>");
-	}
-}
+		fulfillSVGs(){
+			var files = $(".svgFiles").val().split(";");
+			if(files.length === 0){
+				return;
+			}
+			for(var i = 0; i < files.length; i++)
+			{
+				var j = i;
+				$(".images").append("<img src='"+files[j]+"'/></br>");
+			}
+		}
 
-Main.prototype.bindEvents = function(callback){
-	$(".submit").on("click", {callback: callback}, this.loadCode);
-	$(".submit").on("click", this.fulfillSVGs);
-}
+		bindEvents(callback){
+			$(".submit").on("click", {
+				callback: callback,
+				context: this
+				}, this.loadCode);
+			$(".submit").on("click", this.fulfillSVGs);
+		}
 
-Main.prototype.getFileExtension = function(filename){
-	return filename.split('.').pop();	
+		getFileExtension(filename){
+			return filename.split('.').pop();	
+		}
 }
